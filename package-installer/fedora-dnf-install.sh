16fe847ee425760983368cb0a30c3a22c6b99b76
Saif Shahriar <89329547+saifshahriar@users.noreply.github.com>
2022-03-17 15:04:53 +0600

    Update fedora-dnf-install.sh

--- package-installer/fedora-dnf-install.sh
+++ package-installer/fedora-dnf-install.sh
@@ -1,1 +1,8 @@
-
+sudo dnf check-update -y
+sudo dnf upgrade -y
+sudo dnf install starship -y
+sudo dnf install bat -y
+sudo dnf install exa
+sudo dnf install lynx
+sudo dnf install less
+sudo dnf install neofetch
