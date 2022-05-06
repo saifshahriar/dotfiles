5e85554024c5a8453e4273ac2cace808fe28a74d
Saif Shahriar <saif.shahriar.pilotbd@gmail.com>
2022-05-07 00:58:48 +0600

    Updated

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,7 @@
 # export ARCHFLAGS="-arch x86_64"
 #
 ### DEPENDENT PROGRAMS ###
+# ani-cli
 # bat 
 # exa
 # reflector
@@ -17,115 +18,111 @@
 	# xrdb
 	# gpg2
 
-### ADDING TO THE PATH
+###########################################
+###	ADDING TO THE PATH		###
+###########################################
 # First line removes the path; second line sets it.  Without the first line,
-# your path gets massive and fish becomes very slow.
+# your path gets massive && fish becomes very slow.
 set -e fish_user_paths
 set -U fish_user_paths $HOME/.local/bin $HOME/Applications $fish_user_paths
 
-### EXPORT ###
-set fish_greeting                                 # Supresses fish's intro message
-set TERM "xterm-256color"                         # Sets the terminal type
+### Windows wslg ###
+# set -gx GDK_SCALE 2 			# if running as wsl 
 
-### SET MANPAGER ###
+###########################################
+### 		EXPORT			###
+###########################################
+set fish_greeting			# Supresses fish's intromessage.
+set TERM "xterm-256color"		# Sets the terminal type.
+set myEditor vim			# Sets vim as the editor globally.
+
+###########################################
+### 	SET MANPAGER 			###
+###########################################
 ### Uncomment only one of these!
 ### "bat" as manpager
-set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
+# set -x MANPAGER "sh -c 'col -bx | bat -l man -p'"
 ### "vim" as manpager
-# set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
+set -x MANPAGER '/bin/bash -c "vim -MRn -c \"set buftype=nofile showtabline=0 ft=man ts=8 nomod nolist norelativenumber nonu noma\" -c \"normal L\" -c \"nmap q :qa<CR>\"</dev/tty <(col -b)"'
 ### "nvim" as manpager
 # set -x MANPAGER "nvim -c 'set ft=man' -"
 
-### SET EITHER DEFAULT EMACS MODE OR VI MODE ###
+###########################################
+### 	 SET EITHER DEFAULT 		###
+###	EMACS MODE OR VI MODE		###
+###########################################
 function fish_user_key_bindings
   # fish_default_key_bindings
   fish_vi_key_bindings
 end
-### END OF VI MODE ###
 
-### AUTOCOMPLETE AND HIGHLIGHT COLORS ###
+###########################################
+### AUTOCOMPLETE && HIGHLIGHT COLORS 	###
+###########################################
 set fish_color_autosuggestion '#7d7d7d'
 set fish_color_command green
 set fish_color_error '#ff6c6b'
 
-
-### ALIAS ###
-
-# Filesystem
-#
-# If you are coming from CMD
-	alias cls="clear"
-	alias dir="exa -al --color=always --group-directories-first"
-	alias del="rm -rfv"
-# 
-# confirm before overwriting something
-	alias mv="mv -vi"
-	alias cp="cp -vi"
-	alias ln="ln -vi"
-	alias rm="rm -v"
-# 
-# cd 	aliases
+###########################################
+### 		ALIASES			###
+###########################################
+### Filesystem
 	alias ..="cd .."
-# 
-# mkdir aliases
 	alias mkdir="mkdir -pv"
-#
-# Config files
-	alias fishrc="vim ~/.config/fish/config.fish" 
-	alias bashrc="vim ~/.bashrc"
-	alias zshrc="vim ~/.zshrc"
-	alias starshiprc="vim ~/.config/starship.toml"
-	alias nanorc="vim ~/.nanorc"
-	alias neofetchrc="vim ~/.config/neofetch/config.conf"
-#
-# Bat 	alias
-	alias bat='bat --theme="Dracula" --style="numbers,changes" --pager="less -FR" --map-syntax h:cpp'
-# Vim 	alias for Vi
-	alias vi="vim"
-
-### Package aliases
-#
-# ? [distro == Debian || Ubuntu]	# Uncomment this block if you use Debian based disto
-# apt 	aliases
-# ? starts
-#	alias update="sudo apt update"
-#	alias upgrade="sudo apt upgrade"
-#	alias sysup="sudo apt update && sudo apt upgrade"
-#	alias install="sudo apt install"
-#	alias remove="sudo apt remove"
-#	alias autoremove="sudo apt autoremove"
-#	alias cleanup="sudo apt remove && sudo apt autoremove"
-# ? ends
-# 
-# ? [distro == Arch] # Uncomment this block if you use Arch based disto
-# ? starts
-# pacman aliases
-	alias install="sudo pacman -S"
-	alias pacsyy="sudo pacman -Syy"
-	alias pacsyu='sudo pacman -Syu'                  # update only standard pkgs
-	alias pacsyyu='sudo pacman -Syyu'                # Refresh pkglist & update standard pkgs
-	alias pacr="sudo pacman -R"
-	alias pacrns="sudo pacman -Rns"
-	alias cleanup="sudo pacman -Rns (pacman -Qtdq)"	 # Removes orphan packages
-	alias pacunlock='sudo rm /var/lib/pacman/db.lck' # remove pacman lock
-# 
-# yay 	aliases
-	alias yays="yay -S" 
-	alias yaysua='yay -Sua --noconfirm'              # update only AUR pkgs (yay)
-	alias yaysyu='yay -Syu --noconfirm'              # update standard pkgs and AUR pkgs (yay)
-# 
-# paru	aliases	
-	alias parsua='paru -Sua --noconfirm'             # update only AUR pkgs (paru)
-	alias parsyu='paru -Syu --noconfirm'             # update standard pkgs and AUR pkgs (paru)
-# ? ends
-
-# ls	aliases
+# Use exa instead of ls.
 	alias l="exa -al --color=always --icons --group-directories-first" 
 	alias ls="exa -al --color=auto --icons --group-directories-first" 
 	alias la="exa -a --color=auto --group-directories-first" 
 	alias ll="exa -l --color=auto --group-directories-first" 
-	alias ll="exa -aT --color=always --icons --group-directories-first" 
+	alias lt="exa -aT --color=always --icons --group-directories-first" 
 	alias l.='exa -a | egrep "^\."'
+
+# If you are coming from CMD. I mean no one does that, but I like these commands.
+	alias cls="clear"
+	alias dir="exa -al --color=always --group-directories-first"
+	alias del="rm -rfv"
+
+# Always confirm before overwriting something
+	alias mv="mv -vi"
+	alias cp="cp -vi"
+	alias ln="ln -vi"
+	alias rm="rm -v"
+
+### Config files
+	alias fishrc="$myEditor ~/.config/fish/config.fish" 
+	alias bashrc="$myEditor ~/.bashrc"
+	alias zshrc="$myEditor ~/.zshrc"
+	alias starshiprc="$myEditor ~/.config/starship.toml"
+	alias nanorc="$myEditor ~/.nanorc"
+	alias neofetchrc="$myEditor ~/.config/neofetch/config.conf"
+
+### Programs
+# Bat 	alias
+	alias bat='bat -f --italic-text=always --theme="gruvbox-dark" --pager="less -FR --RAW-CONTROL-CHARS --quit-if-one-screen --mouse" --map-syntax "*.ino:C++"  --map-syntax h:cpp'
+# grep	alias. Colorize grep output (good for log files)
+	alias grep='grep --color=auto'
+	alias egrep='egrep --color=auto'
+	alias fgrep='fgrep --color=auto'
+	alias rg='rg -p'
+# editor
+	alias edit='echo "$myEditor is currently set as your default editor. If you want to change it, then edit the fish config file at $HOME/.config/fish/config.fish"; $myEditor'
+	alias nv="nvim"
+	alias vi="vim"
+# media
+	# Watch anime:
+	alias anime='ani-cli'
+	alias animec='ani-cli -c'	# continue watching anime 
+
+	# youtube-dl
+	alias yta-aac="youtube-dl --extract-audio --audio-format aac "
+	alias yta-best="youtube-dl --extract-audio --audio-format best "
+	alias yta-flac="youtube-dl --extract-audio --audio-format flac "
+	alias yta-m4a="youtube-dl --extract-audio --audio-format m4a "
+	alias yta-mp3="youtube-dl --extract-audio --audio-format mp3 "
+	alias yta-opus="youtube-dl --extract-audio --audio-format opus "
+	alias yta-vorbis="youtube-dl --extract-audio --audio-format vorbis "
+	alias yta-wav="youtube-dl --extract-audio --audio-format wav "
+	alias ytv-best="youtube-dl -f bestvideo+bestaudio "
 
 # get fastest mirrors
 	alias mirrorbd="sudo reflector -c Bangladesh >> /etc/pacman.d/mirrorlist"
@@ -134,12 +131,6 @@ set fish_color_error '#ff6c6b'
 	alias mirrord="sudo reflector --latest 50 --number 20 --sort delay --save /etc/pacman.d/mirrorlist"
 	alias mirrors="sudo reflector --latest 50 --number 20 --sort score --save /etc/pacman.d/mirrorlist"
 	alias mirrora="sudo reflector --latest 50 --number 20 --sort age --save /etc/pacman.d/mirrorlist"
-	
-# Colorize grep output (good for log files)
-	alias grep='grep --color=auto'
-	alias egrep='egrep --color=auto'
-	alias fgrep='fgrep --color=auto'
-	alias rg='rg -p'
 
 # adding flags
 	alias df='df -h'                          # human-readable sizes
@@ -179,48 +170,70 @@ set fish_color_error '#ff6c6b'
 	alias gpg-check="gpg2 --keyserver-options auto-key-retrieve --verify"
 # receive the key of a developer
 	alias gpg-retrieve="gpg2 --keyserver-options auto-key-retrieve --receive-keys"
-
-# Play audio files in current dir by type
-#	alias playwav='deadbeef *.wav'
-#	alias playogg='deadbeef *.ogg'
-#	alias playmp3='deadbeef *.mp3'
-#
-# Play video files in current dir by type
-	alias playavi='vlc *.avi'
-	alias playmov='vlc *.mov'
-	alias playmp4='vlc *.mp4'
 	
-# youtube-dl
-	alias yta-aac="youtube-dl --extract-audio --audio-format aac "
-	alias yta-best="youtube-dl --extract-audio --audio-format best "
-	alias yta-flac="youtube-dl --extract-audio --audio-format flac "
-	alias yta-m4a="youtube-dl --extract-audio --audio-format m4a "
-	alias yta-mp3="youtube-dl --extract-audio --audio-format mp3 "
-	alias yta-opus="youtube-dl --extract-audio --audio-format opus "
-	alias yta-vorbis="youtube-dl --extract-audio --audio-format vorbis "
-	alias yta-wav="youtube-dl --extract-audio --audio-format wav "
-	alias ytv-best="youtube-dl -f bestvideo+bestaudio "
-	
-# switch between shells
+# change login shells
 	alias tobash="sudo chsh $USER -s /bin/bash && echo 'Now log out.'"
 	alias tozsh="sudo chsh $USER -s /bin/zsh && echo 'Now log out.'"
 	alias tofish="sudo chsh $USER -s /bin/fish && echo 'Now log out.'"
 
-# Special aliases for this particular system
-#	alias systemWSL="/mnt/d/systemWSL/"
-#	alias systemKali="/mnt/d/systemWSL/systemKali"
-#	alias systemArch="/mnt/d/systemWSL/systemArch"
+### Package aliases
+#
+# ? [distro == Debian || Ubuntu]	# Uncomment this block if you use Debian based disto
+# apt 	aliases
+# ? starts
+#	alias update="sudo apt update"
+#	alias upgrade="sudo apt upgrade"
+#	alias sysup="sudo apt update && sudo apt upgrade"
+#	alias install="sudo apt install"
+#	alias remove="sudo apt remove"
+#	alias autoremove="sudo apt autoremove"
+#	alias cleanup="sudo apt remove && sudo apt autoremove"
+# ? ends
+# 
+# ? [distro == Arch] # Uncomment this block if you use Arch based disto
+# ? starts
+# pacman aliases
+	alias install="sudo pacman -S"
+	alias pacsyy="sudo pacman -Syy"
+	alias pacsyu='sudo pacman -Syu'                  # update only standard pkgs
+	alias pacsyyu='sudo pacman -Syyu'                # Refresh pkglist & update standard pkgs
+	alias pacr="sudo pacman -R"
+	alias pacrns="sudo pacman -Rns"
+	alias cleanup="sudo pacman -Rns (pacman -Qtdq)"	 # Removes orphan packages
+	alias pacunlock='sudo rm /var/lib/pacman/db.lck' # remove pacman lock
+# 
+# yay 	aliases
+	alias yays="yay -S" 
+	alias yaysua='yay -Sua --noconfirm'              # update only AUR pkgs (yay)
+	alias yaysyu='yay -Syu --noconfirm'              # update standard pkgs && AUR pkgs (yay)
+# 
+# paru	aliases	
+	alias parsua='paru -Sua --noconfirm'             # update only AUR pkgs (paru)
+	alias parsyu='paru -Syu --noconfirm'             # update standard pkgs && AUR pkgs (paru)
+# ? ends
 
-### RANDOM COLOR SCRIPT ###
-# Get this script from my GitLab: gitlab.com/dwt1/shell-color-scripts
-# Or install it from the Arch User Repository: shell-color-scripts
-#colorscript random
+###########################################
+###	ADD CUSTOM ALIASES BELOW	###
+###########################################	
+### Special aliases for this particular system
+#	alias systemWSL="cd /mnt/d/systemWSL/"
+#	alias systemKali="cd /mnt/d/systemWSL/systemKali"
+#	alias systemArch="cd /mnt/d/systemWSL/systemArch"
+#	alias win="cd /mnt/c/Users/Saif"
 
-### SETTING THE STARSHIP PROMPT ###
-#starship init fish | source
-
-### Run neofetch at shell startup ###
-#neofetch
-	
-### Windows wslg ###
-#set -gx GDK_SCALE 2 #if your linux is running as wsl 
+########################################### Run these programs whenever the 
+###		STARTUP			### shell starts. NOTE: This may slow
+########################################### down the shell during startup.
+### SETTING THE STARSHIP PROMPT ###	# NOTE: starship prompt is somewhat slow
+# starship init fish | source		# in the fish shell. Use oh-my-fish or 
+					# fisher plugin manager instead.
+# Run either one of them
+# colorscript random			# Random Color Script
+					# Get this script from my GitLab: gitlab.com/dwt1/shell-color-scripts
+					# Or install it from the Arch User Repository: yay -S shell-color-scripts
+# neofetch				# Neofetch, system information fetching tool.
+# fastfetch				# neofetch but written in C. Faster &&
+					# better performance. Use it instead of
+					# neofetch in Arch Linux.
+# pfetch				# Minimalist neofetch.
+					# Install it from AUR: yay -S pfetch-btw.
