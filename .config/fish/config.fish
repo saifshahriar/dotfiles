42c6e48baad3cdb779599e709ef66da59d16f577
Saif Shahriar <sh.saif@proton.me>
2024-09-03 23:37:18 +0600

    add a prompt for fish shell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -164,6 +164,85 @@ set -g fish_pager_color_secondary_completion $foreground
 set -g fish_pager_color_secondary_description $comment
 
 ###########################################
+###               PROMPT                ###
+###########################################
+function fish_prompt --description 'Write out the prompt'
+	set -l last_pipestatus $pipestatus
+	set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
+
+	# implement git stuff if you wish to
+	#if not set -q __fish_git_prompt_show_informative_status
+	#    set -g __fish_git_prompt_show_informative_status 1
+	#end
+	#if not set -q __fish_git_prompt_hide_untrackedfiles
+	#    set -g __fish_git_prompt_hide_untrackedfiles 1
+	#end
+	#if not set -q __fish_git_prompt_color_branch
+	#    set -g __fish_git_prompt_color_branch magenta --bold
+	#end
+	#if not set -q __fish_git_prompt_showupstream
+	#    set -g __fish_git_prompt_showupstream informative
+	#end
+	#if not set -q __fish_git_prompt_color_dirtystate
+	#    set -g __fish_git_prompt_color_dirtystate blue
+	#end
+	#if not set -q __fish_git_prompt_color_stagedstate
+	#    set -g __fish_git_prompt_color_stagedstate yellow
+	#end
+	#if not set -q __fish_git_prompt_color_invalidstate
+	#    set -g __fish_git_prompt_color_invalidstate red
+	#end
+	#if not set -q __fish_git_prompt_color_untrackedfiles
+	#    set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+	#end
+	#if not set -q __fish_git_prompt_color_cleanstate
+	#    set -g __fish_git_prompt_color_cleanstate $green --bold
+	#end
+
+	set -l color_cwd
+	set -l suffix
+	if functions -q fish_is_root_user; and fish_is_root_user
+		if set -q fish_color_cwd_root
+			set color_cwd $fish_color_cwd_root
+		else
+			set color_cwd $fish_color_cwd
+		end
+		set suffix '#'
+	else
+		set color_cwd $fish_color_cwd
+		set suffix '󰡜'
+	end
+
+	# PWD
+	set_color $fish_color_user
+	echo -n (whoami)
+	set_color normal
+	echo -n "@"
+	set_color $fish_color_host
+	echo -n (hostname)
+	echo -n " "
+	set_color normal
+	set_color $color_cwd
+	echo -n (prompt_pwd)
+	set_color normal
+
+	printf '%s ' (fish_vcs_prompt)
+
+	set -l status_color (set_color $fish_color_status)
+	set -l statusb_color (set_color --bold $fish_color_status)
+	set -l prompt_status (__fish_print_pipestatus "[" "]" "|" "$status_color" "$statusb_color" $last_pipestatus)
+	echo -n $prompt_status
+	set_color normal
+
+	if test $__fish_last_status -eq 0
+		printf "%s󰡜%s " (set_color $fish_color_host) (set_color normal)
+	else
+		printf "%s✘%s " (set_color --bold $fish_color_status) (set_color normal)
+	end
+	#echo -n "$suffix "
+end
+
+###########################################
 ###          CUSTOM FUNCTIONS           ###
 ###########################################
 # Functions needed for !! and !$
