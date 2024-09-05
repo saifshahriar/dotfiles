b24bde10a4ab52f0f201b8e7b44d2a6b7769e9ce
Saif Shahriar <sh.saif@proton.me>
2024-09-05 19:31:24 +0600

    correct XDG_RUNTIME_DIR specification

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,7 +25,7 @@ set -gx XDG_DATA_HOME $HOME/.local/share
 set -gx XDG_CONFIG_HOME $HOME/.config
 set -gx XDG_STATE_HOME $HOME/.local/state
 set -gx XDG_CACHE_HOME $HOME/.cache
-#set -gx XDG_RUNTIME_DIR /run/user/"$UID"
+set -gx XDG_RUNTIME_DIR /run/user/(id -u)
 
 set -gx ANDROID_USER_HOME "$XDG_DATA_HOME"/android
 set -gx BAT_THEME "tokyonight_night"
