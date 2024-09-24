991e41e7b5b3e147ca0f24063916e83d80fdd8f0
Saif Shahriar <sh.saif@proton.me>
2024-09-24 23:28:50 +0600

    add python template for cp

---
+++ .vim/snippets/cp/py_template.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+# Author: Saif Shahriar
+
+def sol():
+
+
+def main():
+    test_cases = int(input())
+    for _ in range(t):
+        sol()
+
+if __name__ == "__main__":
+    main()
+
