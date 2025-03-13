3a7102381a996957d62b69440bfa2e9444b3a61d
Saif Shahriar <sh.saif@proton.me>
2025-03-13 21:58:25 +0600

    cpp_template.cpp: remove unnecessary macros

--- .vim/snippets/cp/cpp_template.cpp
+++ .vim/snippets/cp/cpp_template.cpp
@@ -10,23 +10,19 @@
 #define _GLIBCXX_DEBUG
 #define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
 #else
-#define debug(x)
 #include <bits/stdc++.h>
+#define debug(x)
 using namespace std;
 #endif
 
 /* macros */
-#define ALL(v)  (v).begin(), (v).end()
-#define INF     INT_MAX
-#define MOD     1e9+7
-#define SUM(v)  (accumulate((v).begin(), (v).end(), 0))
-#define el      '\n'
-#define sz(a)   ((int)((a).size()))
-#define fixprec fixed << setprecision
+#define ALL(v) (v).begin(), (v).end()
+#define MOD    1e9+7
+#define SUM(v) accumulate((v).begin(), (v).end(), 0)
+#define el     '\n'
+#define sz(a)  ((int)((a).size()))
 
 /* typedefs */
-#define char   unsigned char
-#define const  constexpr
 #define ll     long long
 #define ull    unsigned long long
 #define ld     long double
